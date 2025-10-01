55ff1fe1768189cb6a77bac0fa65a8855f10aafd
kurumayoman <kurumayoman@gmail.com>
2025-10-01 18:03:16 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -4,3 +4,20 @@ flower.append(i)
 
 for i in range(1,51):
     print(i)
+    # i=1
+# for i in range(1,6):
+#     print(i)
+
+# i=0
+# while i<51:
+#     print(i)
+#     i=i+2
+# n=int(input("enter a num:"))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+name1=input("enter your name:")
+name2=input("enter your :) name:")
+print(name1)
+print(name2)
